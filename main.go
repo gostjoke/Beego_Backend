7639d2bdbdafe785141f49189aba53ffaf2e5e34
Will <gostjoketw@gmail.com>
2025-10-01 22:23:41 -0700

    Redis version

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 import (
 	// 引用 models 包
 	_ "Beego_Backend/routers"
+	"context"
+	"fmt"
 
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/beego/beego/v2/client/orm"
 	beego "github.com/beego/beego/v2/server/web"
 	_ "github.com/go-sql-driver/mysql" // 匿名導入 MySQL 驅動
+	"github.com/redis/go-redis/v9"
 )
+
+var (
+	ctx = context.Background()
+	Rdb *redis.Client
+)
+
+func initRedis() {
+	Rdb = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Redis 預設 port
+		Password: "",               // 如果有設定密碼要填
+		DB:       0,                // 使用 DB0
+	})
+
+	// 測試連線
+	pong, err := Rdb.Ping(ctx).Result()
+	if err != nil {
+		panic(fmt.Sprintf("Redis 連線失敗: %v", err))
+	}
+	fmt.Println("連線成功:", pong)
+}
 
 func init() {
 	// 註冊 MySQL 驅動
@@ -24,6 +47,9 @@ func init() {
 
 	// 自動創建表 only when you need to create table
 	orm.RunSyncdb("default", false, true) // 第二個參數表示是否強制重新創建表，第三個參數表示是否輸出詳細信息
+
+	// 初始化 Redis
+	initRedis()
 }
 
 func main() {
